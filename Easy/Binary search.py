ff05465eac9a68547c7fb40161fdd263f2c2822a
OM AUTI <omauti85@gmail.com>
2025-10-07 19:09:04 +0530

    Update Binary search.py

--- Easy/Binary search.py
+++ Easy/Binary search.py
@@ -9,7 +9,7 @@ class Solution:
             if nums[mid] == target:
                 return mid
             elif nums[mid]< target:
-                left +=1
+                left=mid+1
             else:
-                right -=1
+                right=mid-1
         return -1
